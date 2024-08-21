f8bc388406227dca953882a9dfbb9a95f2e6f00b
Mark Skelton <markskelton10@gmail.com>
2024-08-21 17:42:55 +0100

    Write shift_letter method
    I wrote it this way, line-by-line and with an unused variable at the
    end, to make it more readable. I also combined it into one method to
    reduce repetition in the code.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,17 +1,15 @@
 def caesar_cipher(input_string, shift_factor)
   input_characters = input_string.split("")
-  p input_characters
 
   output_characters = input_characters.map do |character|
     if in_downcase_alphabet?(character)
-      shift_downcase_letter(character, shift_factor)
+      shift_letter(character, shift_factor, "a")
     elsif in_upcase_alphabet?(character)
-      shift_upcase_letter(character, shift_factor)
+      shift_letter(character, shift_factor, "A")
     else
       character
     end
   end
-  p output_characters
 
   output_characters.join
 end
@@ -24,12 +22,11 @@ def in_upcase_alphabet?(character)
   ("A".ord.."Z".ord).include?(character.ord)
 end
 
-def shift_downcase_letter(letter, shift_factor)
-  letter
-end
-
-def shift_upcase_letter(letter, shift_factor)
-  letter
+def shift_letter(letter, shift_factor, a_or_A)
+  number_representing_letter_from_0_to_25 = letter.ord - a_or_A.ord
+  shifted_number = number_representing_letter_from_0_to_25 + shift_factor
+  shifted_number_from_0_to_25 = shifted_number % 26
+  shifted_letter = (shifted_number_from_0_to_25 + a_or_A.ord).chr
 end
 
 puts "Please enter an input string and shift factor:"
