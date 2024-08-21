7f02955f29aea515f8a7ba358b51186f74c2749b
Mark Skelton <markskelton10@gmail.com>
2024-08-21 18:26:27 +0100

    Change split() to chars() to get character array
    This is easier to read in my opinion.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,7 +1,5 @@
 def caesar_cipher(input_string, shift_factor)
-  input_characters = input_string.split("")
-
-  output_characters = input_characters.map do |character|
+  output_characters = input_string.chars.map do |character|
     if character.between?("a", "z")
       # ASCII value converted to range 0-25, shifted (wrap z-a), then converted back
       ((character.ord - "a".ord + shift_factor) % 26 + "a".ord).chr
