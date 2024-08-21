a51270edc6410309541d5390ebe19a2a9ba6bede
Mark Skelton <markskelton10@gmail.com>
2024-08-21 16:46:19 +0100

    Create if-else for if character is in alphabet

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,18 +1,30 @@
-def caesar_cipher(input_string, shift)
+def caesar_cipher(input_string, shift_factor)
   input_characters = input_string.split("")
   p input_characters
 
   output_characters = input_characters.map do |character|
-    original_number = character.ord
+    if in_alphabet?(character)
+      shift_letter(character, shift_factor)
+    else
+      character
+    end
   end
   p output_characters
 
   input_string
 end
 
-puts "Please enter an input string and shift number:"
-input_string = gets.chomp
-shift = gets.chomp.to_i
+def in_alphabet?(character)
+  ("A".ord.."Z".ord).include?(character.ord) || ("a".ord.."z".ord).include?(character.ord)
+end
 
-output_string = caesar_cipher(input_string, shift)
+def shift_letter(letter, shift_factor)
+  letter
+end
+
+puts "Please enter an input string and shift factor:"
+input_string = gets.chomp
+shift_factor = gets.chomp.to_i
+
+output_string = caesar_cipher(input_string, shift_factor)
 puts "The modified string is: #{output_string}"
