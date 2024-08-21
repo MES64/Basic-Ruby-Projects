e4e6fc7ddac5833c590521f2d964c9660da5893e
Mark Skelton <markskelton10@gmail.com>
2024-08-21 16:16:59 +0100

    Output result from caesar_cipher
    For now the output string is the input string

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,10 @@
+def caesar_cipher(input_string, shift)
+  input_string
+end
+
 puts "Please enter an input string and shift number:"
 input_string = gets.chomp
 shift = gets.chomp.to_i
+
+output_string = caesar_cipher(input_string, shift)
+puts "The modified string is: #{output_string}"
