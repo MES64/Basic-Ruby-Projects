ee0b9c099efee12f2f7c53522d6ee3fdfbd80a66
Mark Skelton <markskelton10@gmail.com>
2024-08-22 09:37:50 +0100

    Add more tests

--- sub_strings.rb
+++ sub_strings.rb
@@ -9,9 +9,33 @@ end
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 test1 = substrings("below", dictionary)
 test2 = substrings("Howdy partner, sit down! How's it going?", dictionary)
+test3 = substrings("below", [])
+test4 = substrings("below", dictionary.map(&:upcase))
+test5 = substrings("BELOW", dictionary.map(&:upcase))
+test6 = substrings("", dictionary)
 
 puts "dictionary = #{dictionary}"
+
+puts "Test for single word in string:"
 puts "substrings('below', dictionary) outputs: #{test1}"
 puts "Correct: #{test1 == { "below" => 1, "low" => 1 }}"
+
+puts "Test for multiple words in string with uppercase letters too:"
 puts "substrings('Howdy partner, sit down! How's it going?', dictionary) outputs: #{test2}"
 puts "Correct: #{test2 == { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }}"
+
+puts "Test for empty dictionary:"
+puts "substrings('below', []) outputs: #{test3}"
+puts "Correct: #{test3 == {}}"
+
+puts "Test for uppercase letters in dictionary:"
+puts "substrings('below', dictionary.map(&:upcase)) outputs: #{test4}"
+puts "Correct: #{test4 == { "BELOW" => 1, "LOW" => 1 }}"
+
+puts "Test for both string and dictionary containing uppercase letters:"
+puts "substrings('BELOW', dictionary.map(&:upcase)) outputs: #{test5}"
+puts "Correct: #{test5 == { "BELOW" => 1, "LOW" => 1 }}"
+
+puts "Test for empty string:"
+puts "substrings('', dictionary) outputs: #{test6}"
+puts "Correct: #{test6 == {}}"
