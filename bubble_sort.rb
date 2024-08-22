21b85fdc43edb740e7805b1a308d373c5ce5a1e8
Mark Skelton <markskelton10@gmail.com>
2024-08-22 13:18:07 +0100

    Add more tests for bubble_sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -16,3 +16,7 @@ end
 array = [4,3,78,2,0,2]
 p bubble_sort(array)  # => [0, 2, 2, 3, 4, 78]
 p array  # => [0, 2, 2, 3, 4, 78] -> It also affects the original array
+
+p bubble_sort([])  # => []
+p bubble_sort([5,4,3,2,1])  # => [1,2,3,4,5]
+p bubble_sort([1,2,3,4,5])  # => [1,2,3,4,5]
